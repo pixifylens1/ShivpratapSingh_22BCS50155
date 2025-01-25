42ca6c4f20c536f7c446570a57035b7a895b0d10
Shivpratap Singh <shivraj1lakh@gmail.com>
2025-01-25 14:20:05 +0530

    Add solution for Valid Palindrome problem

---
+++ LT-125_Valid_Palindrome.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+bool isValidPalindrome(const   string& s) {
+    int i = 0, j = s.size() - 1;
+    while (i < j) {
+        if (!isalnum(s[i])) {
+            ++i;
+        } else if (!isalnum(s[j])) {
+            --j;
+        } else if (tolower(s[i]) != tolower(s[j])) {
+            return false;
+        } else {
+            ++i;
+            --j;
+        }
+    }
+    return true;
+}
+
+int main() {
+      string s = "A man, a plan, a canal: Panama";
+    if (isValidPalindrome(s)) {
+          cout << "The string is a valid palindrome." <<   endl;
+    } else {
+          cout << "The string is not a valid palindrome." <<   endl;
+    }
+    return 0;
+}
